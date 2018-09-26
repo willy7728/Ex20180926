68a5b3c4792179e45d6c167ea5513ad48f925607
willy7728 <107021050@gm.asia.edu.tw>
2018-09-26 16:00:13 +0800

    HOMEWORK

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        #5
+//        Scanner scn = new Scanner(System.in);
+//        char ch = scn.next().charAt(0);
+//        System.out.println(Integer.toHexString((int)ch));
+//        System.out.println((int)ch);
+
+//        Scanner scn = new Scanner(System.in);
+//        String str = scn.next();
+//        int v1 = Integer.parseInt(str, 16 );
+//        System.out.println((char)v1);
+
+//        Scanner scn = new Scanner(System.in);
+//        int v1 = scn.nextInt();
+//        System.out.println(Math.sqrt(v1)*10);
+
+//        Scanner scn = new Scanner(System.in);
+//        int h = scn.nextInt();
+//        int type = scn.nextInt();
+//        if (type == 1){
+//            System.out.println((h - 80)*0.7f);
+//        }else {
+//            System.out.println((h - 70)*0.6f);
+//        }
+
+//        System.out.println((Math.round((Math.PI)*100))/10f);
+
+//        Scanner scn = new Scanner(System.in);
+//        int     v1 = scn.nextInt(),
+//                v2 = scn.nextInt(),
+//                v3 = scn.nextInt(),
+//                v4 = scn.nextInt();
+//        int r = v1 - (v2*15+v3*20+v4*30);
+//        if (r<0){
+//            System.out.println(0);
+//        }else {
+//            System.out.println(r/50);
+//            r = r%50;
+//            System.out.println(r/5);
+//            System.out.println(r%5);
+//        }
+
+        Scanner scn = new Scanner(System.in);
+        int     x = scn.nextInt(),
+                y = scn.nextInt();
+        double d = Math.
+
+    }
+}
